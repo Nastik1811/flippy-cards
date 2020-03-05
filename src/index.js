0ceb964f704ba84a7413a7cef6eae22e7cb3e2f4
Anastasia Lugovskaya <anastasia@Anastasias-MacBook-Pro.local>
2020-03-05 10:59:43 +0300

    add some css styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div></div>, document.getElementById('container'));
+import './style.css';
+import './assets/fonts/Roboto-Regular.ttf'
+import './assets/fonts/Comfortaa-Regular.ttf'
+import './assets/images/Clouds.png'
+import CollectionPreviewsContainer from './Components/CollectionPreviewsContainer'
+import Card from './Components/Card'
+
+ReactDOM.render(<CollectionPreviewsContainer/>, document.getElementById('root'));
 
