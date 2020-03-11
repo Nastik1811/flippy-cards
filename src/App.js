63dffe3169f6d1edc9b780064f143c4fca1ea553
Anastasia Lugovskaya <anastasia@Anastasias-MacBook-Pro.local>
2020-03-11 20:42:22 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import Session from './views/Session'
 import Navbar from './Components/Navbar'
 
 import { Route, Link, Switch, Router } from 'react-router-dom'
+import CardEditor from './views/CardEditor'
+import { Landing } from './views/Landing'
 
+let isAutorized = true;
 
 const App = () => {
     return (
         <div className="app-container cloud-background">
-            <Navbar/>
+            <Navbar isAutorized={isAutorized}/>
             <div className="main-container">
                 <Switch>
-                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/' component={Landing}/>
+                    <Route exact path='/home' component={Home}/>
                     <Route exact path='/manage/' component={Manage}/> 
-                    <Route exact path='/session'component={Session}/>
+                    <Route exact path='/session' component={Session}/>
+                    <Route exact path='/session/:slug' component={Session}/>
+                    <Route exact path='/editor/:slug' component={CardEditor}/>
                     <Route component={Error}/> 
                 </Switch>
             </div>
