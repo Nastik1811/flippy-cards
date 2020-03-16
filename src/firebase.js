0777774c2a6263dba3ca61d89fde358b4b3b79dc
Nastik1811 <lugovskaya.1999@mail.ru>
2020-03-16 14:56:03 +0300

    Add auth provider

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import app from 'firebase/app';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebaseui'
 
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -7,11 +9,10 @@ const config = {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
 
-  class Firebase {
-    constructor() {
-      app.initializeApp(config);
-    }
-  }
-  export default Firebase;
+const app = firebase.initializeApp(config);
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -10,15 +10,13 @@ import './assets/fonts/Roboto-Medium.ttf';
 import './assets/fonts/Comfortaa-Regular.ttf';
 import './assets/images/Clouds.png';
 import App from './App'
-
-import Firebase, { FirebaseContext } from './components/Firebase';
+import { AuthProvider } from './Auth';
 
 ReactDOM.render(
-<Router>
-    <FirebaseContext.Provider value={new Firebase()}>
-             <App/>
-    </FirebaseContext.Provider>
-</Router>, 
-
-document.getElementById('root'));
+    <AuthProvider>
+        <Router>
+            <App/>
+        </Router>
+    </AuthProvider>,
+     document.getElementById('root'));
 
