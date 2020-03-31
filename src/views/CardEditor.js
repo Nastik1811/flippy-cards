116e3add8f95a1c8a9a8f64887f3f04b0b0e15dc
Nastik1811 <lugovskaya.1999@mail.ru>
2020-03-31 17:47:17 +0300

    Add card editor

--- src/views/CardEditor.js
+++ src/views/CardEditor.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-
+import { useParams } from 'react-router-dom'
+import { useForm } from 'react-hook-form';
+import app from '../firebase';
+import { Redirect } from 'react-router-dom';
 
 const EditorWindow = props => {
     return (
@@ -9,14 +12,44 @@ const EditorWindow = props => {
     )
 }
 
-
-
-export default class CardEditor extends Component {
-    render() {
-        return (
-            <EditorWindow>
-                <div className="edit-card-content">Edit card here</div>
-            </EditorWindow>
-        )
+const CardEditor = () => {
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = data => {
+        // ????? app.auth().currentUser.uid || currentUser.uid from AuthContext || какой-нибудь redax ?????
+        
+        app.firestore().collection('users').doc(app.auth().currentUser.uid).collection('cards').add({
+            front: data.front,
+            back: data.back,
+            collection: data.collection,
+            created: new Date(),
+            nextRecall: new Date(),
+            learningStage: 0,
+          }).catch(function(error) {
+            console.error('Error writing new message to Firebase Database', error);
+          });
     }
+    
+    console.log(errors);
+
+    return (
+            <div className="editor">
+                Edit card here
+                <form onSubmit={handleSubmit(onSubmit)} className="card-editor-form">
+                    <textarea placeholder="front side" name="front" ref={register({required: true, maxLength: 255})} />
+                    <textarea placeholder="back side" name="back" ref={register({required: true, maxLength: 255})} />
+                    <label>
+                        Choose a collection: 
+                        <select name="collection" ref={register}>
+                            <option>Js</option>
+                            <option>English words</option>
+                            <option>React</option>
+                        </select>
+                    </label>
+                    <input type="submit" />
+                </form>
+                
+            </div>
+    )
 }
+
+export default CardEditor;
