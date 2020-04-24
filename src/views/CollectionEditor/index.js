89d6ffaebdcc2322a0f5432ff0b817b0fb0beef6
Nastik1811 <lugovskaya.1999@mail.ru>
2020-04-24 16:51:44 +0300

    Refactor update function

--- src/views/CollectionEditor/index.js
+++ src/views/CollectionEditor/index.js
@@ -11,38 +11,35 @@ const CheckablePreview = ({children}) => {
 } 
 
 const CollectionEditor = () => {
-
     let {slug} = useParams();
     const {manager} = useContext(DataContext);
+
     const [collection, setCollection] = useState(null);
     const [checkedCards, setCheckedCards] = useState([]);
     const [uncheckedCards, setUncheckedCards] = useState([]);
 
     useEffect(() => {
-        async function fetchData(){
-            await manager.getCards(slug).then(data => setCheckedCards(data));
-            await manager.getCardsWithoutCollection().then(data => setUncheckedCards(data));
-            await manager.getCollection(slug).then(data => setCollection(data));
+        function fetchData(){
+            manager.getCards(slug).then(data => setCheckedCards(data));
+            manager.getCardsWithoutCollection().then(data => setUncheckedCards(data));
+            manager.getCollection(slug).then(data => setCollection(data));
         }
         fetchData();
     }, [manager, slug])
 
     return (
-        <>
-            <div className={styles["editor"]}>
-                <header>
-                    <h1 className={styles["name"]}>{collection? collection.name: "" }</h1>
-                    <button className={styles["save-btn"]}> Save</button>
-                </header>
-                <div className={styles["scroll"]}>
-                    <div className={styles["cards"]}>
-                        {checkedCards.map(c => <CardItem front={c.content.front} back={c.content.back} key={c.id}/>)}
-                        {uncheckedCards.map(c => <CardItem front={c.content.front} back={c.content.back} key={c.id}/>)}
-                    </div>
+        <div className={styles["editor"]}>
+            <header>
+                <h1 className={styles["name"]}>{collection? collection.name: "" }</h1>
+                <button className={styles["save-btn"]}> Save</button>
+            </header>
+            <div className={styles["scroll"]}>
+                <div className={styles["cards"]}>
+                    {checkedCards.map(c => <CardItem front={c.content.front} back={c.content.back} key={c.id}/>)}
+                    {uncheckedCards.map(c => <CardItem front={c.content.front} back={c.content.back} key={c.id}/>)}
                 </div>
             </div>
-            
-        </>
+        </div>
     )
   
 }
