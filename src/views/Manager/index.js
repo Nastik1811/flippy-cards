9268fe66458b68394e2b07c19ac9021a5c95420f
Nastik1811 <lugovskaya.1999@mail.ru>
2020-04-24 19:55:56 +0300

    Manager component redesign

--- src/views/Manager/index.js
+++ src/views/Manager/index.js
@@ -1,65 +1,23 @@
 import React, { useState, useEffect, useContext } from 'react'
 import CardPreview from './CardPreview'
-import { Link } from 'react-router-dom'
 import CollectionPreview from './CollectionPreview'
 import styles from './Manager.module.scss'
 import { DataContext } from '../../DataManger'
 
-const Tabbar = ({children}) => {
+import {
+    Switch,
+    Route,
+    NavLink,
+    useRouteMatch,
+    Link
+  } from "react-router-dom";
+  
+import NewCollectionForm from '../NewCollectionForm'
+import CardEditor from '../CardEditor'
 
-    return (
-        <nav className={styles["tabbar"]}>
-            <ul className={styles["tab-links"]}>
-                {children}
-            </ul>
-            <hr/>
-        </nav>
-    )
-}
-
-const Tab = ({active, index, label, onClick}) => {
-
-    let className = active ? styles["tab-active"] : styles["tab"];
-    return (
-        <li className={className} onClick={()=>{onClick(+index)}}>{label}</li>
-    )
-}
-
-const CardsPanel = ({cards}) => {
-    return (
-        cards ?
-            <div className={styles["board"]}>
-                <Link to="/new" className={styles["add-btn"]}> + </Link>
-                {cards.map(c => <CardPreview slug={c.id} front={c.content.front} back={c.content.back} key={c.id} />)}
-            </div> 
-            : 
-            <div>Loading...</div>
-    )
-}
-
-const CollectionsPanel = ({collections, match}) => {
-    return (
-        collections ?
-            <div className={styles["board"]}>
-                <Link to={match.url + "/new"}  className={styles["add-btn"]}> + </Link>
-                {collections.map(c => <CollectionPreview slug={c.id} name={c.name} key={c.id}/>)}
-            </div>
-            : 
-            <div>Loading...</div>
-    )
-}
-
-//I'm still thinking about suggested approach to manage collections and cards separately via corresponding urls
-//But for now I'd like to solve other problems first
-
-const Manager = ({match}) => {
+const CardsPanel = () => {
     const {manager} = useContext(DataContext);
-
-    const [collections, setCollections] = useState([]);
-    useEffect(() => {
-        manager.getCollections().then(data => setCollections(data))
-    }, [manager]
-    )
+    let match = useRouteMatch();
 
     const [cards, setCards] = useState([]);
     useEffect(() => {
@@ -67,18 +25,48 @@ const Manager = ({match}) => {
     }, [manager]
     )
 
-    const [currentTab, setCurrentTab] = useState(0);
-
-    return (
-        <>
-            <Tabbar>
-                <Tab index="0" active={currentTab === 0 ? true : false}  label="Collections" onClick={index => setCurrentTab(index)}/>
-                <Tab index="1" active={currentTab === 1 ? true : false}  label="Cards" onClick={index => setCurrentTab(index)} />
-            </Tabbar>
-            {currentTab === 0 ? <CollectionsPanel collections={collections} match={match}/> : <CardsPanel cards={cards} />}
-        </>
-    )
+    return cards ?
+            (<div className={styles["board"]}>
+                <Link to="/new" className={styles["add-btn"]}> + </Link>
+                {cards.map(c => <CardPreview slug={c.id} front={c.content.front} back={c.content.back} key={c.id} />)}
+            </div> )
+            : 
+            (<div>Loading...</div>)
 }
 
+const CollectionsPanel = () => {
+    const {manager} = useContext(DataContext);
+    let match = useRouteMatch();
+    const [collections, setCollections] = useState([]);
+
+    useEffect(() => {
+        manager.getCollections().then(data => setCollections(data))
+    }, [manager])
+
+    return collections ?
+            (<div className={styles["board"]}>
+                <Link to={match.url + "/new"}  className={styles["add-btn"]}> + </Link>
+                {collections.map(c => <CollectionPreview slug={c.id} name={c.name} key={c.id}/>)}
+                <Route path={match.path + "/new"} children={<NewCollectionForm/>} />
+            </div>)
+            : 
+            (<div>Loading...</div>)
+}
+
+const Manager = () => {
+    let match = useRouteMatch();
+    return (
+        <section>
+            <nav className={styles["tabbar"]}>
+                <NavLink to={match.url + "/collections"} className={styles["tab"]} activeClassName={styles["tab-active"]} >Collections</NavLink>
+                <NavLink to={match.url + "/cards"}  className={styles["tab"]} activeClassName={styles["tab-active"]}>Cards</NavLink>
+            </nav>
+            <Switch>
+                <Route path={match.path + "/collections"} children={<CollectionsPanel/>}/>
+                <Route path={match.path + "/cards"} children={<CardsPanel/>} />
+            </Switch>
+        </section>
+    )
+}
 
 export default Manager;
