97fcda6d5c6632c261677c44865da3b8330821de
Nastik1811 <lugovskaya.1999@mail.ru>
2020-04-27 19:11:48 +0300

    Split card editor

--- src/views/CardEditor/index.js
+++ src/views/CardEditor/index.js
@@ -1,65 +1,135 @@
-import React, {useCallback, useContext, useState, useEffect} from 'react'
-import {withRouter} from 'react-router-dom'
+import React, {useCallback, useContext, useState, useEffect, useMemo} from 'react'
+import {withRouter, useParams} from 'react-router-dom'
 import styles from './CardEditor.module.scss'
 import { DataContext } from '../../DataManger';
+import SubmitButton from '../../components/SubmitButton';
 
-const CardEditor = ({history}) => {
+const CardCreateContainer= ({history}) => {
   const {manager} = useContext(DataContext);
-  const [collections, setCollections] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => await manager.getCollections().then(data => setCollections(data));
-    fetchData()
-  }, [manager])
+  const initialDetails = {
+      content: {
+        front: "", 
+        back: ""
+      },
+      collection: {
+        id: null,
+        name: ""
+      }
+    }
 
-  const handleSubmit = useCallback(
-      async event => {
-        event.preventDefault();
-        const { front, back, collection_id} = event.target.elements;
-      
-        const id = collection_id.value;
-        console.log(id)
-        const collection = id? {
-          id,
-          name: collections.find(collection => collection.id === id).name
-        } : null;
+  const onSubmit = async (newDetails) => {
+    try {
+      await manager.addCard(newDetails);
+      history.push('/home');
+    } catch (error) {
+      alert(error);
+    }
+  }
 
-        await manager.addCard({
-          content: {
-            front: front.value,
-            back: back.value
-          },
-          collection
-        })
-
-        history.push("/home");
-      }, [history, manager, collections] );
-
-    return (
-            <div className={styles["editor"]}>
-                <form onSubmit={handleSubmit} className={styles["card-editor-form"]}>
-                    <div className={styles["row-flex-container"]}>
-                        <div className={styles["edit-container"]}>
-                             <label>FRONT SIDE</label>
-                            <textarea placeholder="front side" name="front" />
-                        </div>
-                        <div className={styles["edit-container"]}> 
-                            <label>BACK SIDE</label>
-                            <textarea placeholder="back side" name="back" />
-                        </div>
-                    </div>
-
-                    <label>
-                        Choose a collection: 
-                        <select name="collection_id" >
-                          {collections.map(c => <option value={c.id} key={c.id}>{c.name}</option>)}
-                        </select>
-                    </label>
-                    <input type="submit" className={styles["submit-btn"]} value="Save" />
-                </form>
-                
-            </div>
-    )
+  return (
+    <div className={styles["editor"]}>
+        <CardForm 
+          initialDetails={initialDetails} 
+          onSubmit={newDetails => onSubmit(newDetails)}/>  
+    </div>
+  )
 }
 
-export default withRouter(CardEditor);
+const CardEditContainer = ({history}) => {
+  const {manager} = useContext(DataContext);
+  const {id} = useParams();
+  const [initialDetails, setInitialDetails] = useState(null);
+  useEffect(() => {
+    manager.getCardDetails(id).then(data => setInitialDetails(
+          {
+            content: data.content, 
+            collection: data.collection
+          })
+        )
+  }, [manager, id])
+
+  const onSubmit = async (newDetails) => {
+    try {
+      await manager.updateCard(id, newDetails)
+      history.push('/home')
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  return (
+    initialDetails ? 
+      (<div className={styles["editor"]}>
+          <CardForm 
+            initialDetails={initialDetails} 
+            onSubmit={newDetails => onSubmit(newDetails)}/>  
+      </div>) : 
+      <div>Loading...</div>
+  )
+
+}
+
+const CardForm = ({initialDetails, onSubmit}) => {
+  const {manager} = useContext(DataContext);
+
+  const [collections, setCollections] = useState([]);
+  const [cardDetails, setCardDetails] = useState(initialDetails);
+  const handleSubmit = () => {
+    //validate data here?
+    onSubmit(cardDetails)
+  }
+
+  useEffect(() => {
+    manager.getCollections().then(data => setCollections(data))
+  }, [manager])
+
+  return(
+    <form onSubmit={handleSubmit} className={styles["card-editor-form"]}>
+            <div className={styles["row-flex-container"]}>
+                <ContentArea label="front" onChange={(front) => setCardDetails({...cardDetails, front})}/>
+                <ContentArea label="back" onChange={(back) => setCardDetails({...cardDetails, back})}/>
+            </div>
+
+            <CollectionSet 
+              collections={collections}
+              value={cardDetails.collection} 
+              onChange={collection => setCardDetails({...cardDetails, collection}) }
+            />
+
+            <SubmitButton label="Save" className={styles["submit-btn"]} />
+        </form>
+  )
+}
+
+//const CardFront = () => {}
+//const CardBack = () => {}
+
+const ContentArea = ({label, value, onChange}) => {
+  return(
+    <div className={styles["edit-container"]}> 
+      <label>{label}</label>
+      <textarea placeholder={label} value={value} onChange={(e) => onChange(e.target.value)}/>
+    </div>
+  )
+}
+
+const CollectionSet = ({collections, value, onChange}) => {
+  // As far as I understand we could pass only primitive values through "target.value"  
+  // But I need both id and name while updating card detail
+  // Is there more appropriate way to get around that?
+  const handleChange = (e) => {
+    const id = e.target.value;
+    const name = collections.find(collection => collection.id === id).name;
+    onChange({id, name}) 
+  }
+
+  return(
+    <label>
+        Choose a collection: 
+        <select value={value} onChange={handleChange}>
+          {collections.map(c => <option value={c.id} key={c.id}>{c.name}</option>)}
+        </select>
+    </label>
+  )
+}
