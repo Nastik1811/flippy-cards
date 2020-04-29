b11281817dd12e4a7d69e67a894041131cfaca1b
Nastik1811 <lugovskaya.1999@mail.ru>
2020-04-29 17:39:10 +0300

    Lift state up inside Home component

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Statistics from './Statistics'
 import ReviewLinksBoard from './ReviewLinksBoard'
 import Invitation from './ReviewInvitation'
 import styles from './Home.module.scss'
+import { DataContext } from '../../DataManger'
 
  const Home = () => { 
+    const {manager} = useContext(DataContext);
+
+    const [userName, setUserName] = useState(null);
+    const [total, setTotal] = useState(0)
+    const [collections, setCollections] = useState([]);
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        let cardsLoading = manager.getTotalRepeatNumber().then(data => setTotal(data));
+        let nameLoading = manager.getUserName().then(name => setUserName(name));
+        let collectionLoading = manager.getCollectionToRepeatPreviews().then(data => setCollections(data))
+
+        Promise.all([cardsLoading, nameLoading, collectionLoading]).then(() => setIsLoading(false));
+
+    }, [manager])
+
+    console.log(isLoading)
+
     return(
+        !isLoading?
         <>
             <section className={styles["main-section"]}>
-                <Link to="/new" className={styles["add-btn"]}> Add card</Link>
-                <Invitation/>
+                <Link to="/card/new" className={styles["add-btn"]}> Add card</Link>
+                <Invitation userName={userName} total={total}/>
             </section>
-            <ReviewLinksBoard/>
+            <ReviewLinksBoard collections={collections}/>
             <Statistics/>
-        </>
+        </> :
+        <div>Loading...</div>
     );
 }
 
