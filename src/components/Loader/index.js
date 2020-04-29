8b737592ae87e11950a151cb97c3ddb220159fe7
Nastik1811 <lugovskaya.1999@mail.ru>
2020-04-29 16:50:43 +0300

    Add Loader and Popup components

---
+++ src/components/Loader/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Loader.module.scss'
+
+const Loader = () => {
+    return(
+        <div>
+            Loading...
+        </div>
+    )
+}
+
+export default Loader;
