24b8d870d90005bc83c260f9d485df6d9277c376
Nastik1811 <lugovskaya.1999@mail.ru>
2020-04-30 13:03:38 +0300

    Fix collection selector to be able set collection to null

--- src/components/FormElements/CollectionSelect/index.js
+++ src/components/FormElements/CollectionSelect/index.js
@@ -4,14 +4,22 @@ const CollectionSelect = ({collections, value, onChange}) => {
 
     const handleChange = (e) => {
       const id = e.target.value;
-      const name = collections.find(collection => collection.id === id).name;
-      onChange({id, name}) 
+      !id ? onChange({
+        id: null, 
+        name: ""
+      }) : 
+      onChange({
+        id, 
+        name: collections.find(collection => collection.id === id).name
+      })
+      
     }
   
     return(
       <label>
-          Choose a collection: 
-          <select value={value.id} onChange={handleChange}>
+          Set a collection: 
+          <select value={value.id ? value.id : ""} onChange={handleChange}>
+            <option value="">None</option>
             {collections.map(c => <option value={c.id} key={c.id}>{c.name}</option>)}
           </select>
       </label>
