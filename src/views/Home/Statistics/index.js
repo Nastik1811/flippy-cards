3b364115b8cca335143123f625a67d949ab2a07a
Nastik1811 <lugovskaya.1999@mail.ru>
2020-05-04 23:23:56 +0300

    Minor styles updates

--- src/views/Home/Statistics/index.js
+++ src/views/Home/Statistics/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default () => {
     return (
         <section>
-            <header className="section-header" >User's statistics will be there.</header>
         </section>
     )
 }
