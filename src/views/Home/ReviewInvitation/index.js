fee8f6ba3748c7f3ed5c4d165b546abbea98c9f8
Nastik1811 <lugovskaya.1999@mail.ru>
2020-05-04 13:01:33 +0300

    Update Home style

--- src/views/Home/ReviewInvitation/index.js
+++ src/views/Home/ReviewInvitation/index.js
@@ -2,21 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './ReviewInvitation.module.scss'
 
-const InvitationMsg = ({userName, amount}) => {
+const InvitationMsg = ({userName, amount, isNewUser}) => {
 
     let message = amount > 0 ? 
-     `You have something to reapet. ${amount} cards are awaiting you.. Let’s start learning!` :
+     `You have something to repeat. ${amount} cards are awaiting you.. Let’s start learning!` :
      `There are no cards ready to repeat. But you can add new one any time! `
 
     return (
-        <>
+        <div className={styles["msg"]} >
             <div className={styles["greeting-msg"]} >
                 Hello, {userName}!
             </div>
             <div className={styles["invitation-msg"]} >
                 {message}
             </div>
-        </>
+        </div>
     )
 }
 
@@ -24,9 +24,9 @@ const ReviewInvitation = ({userName, total}) => {
     let isNotNull= total > 0
 
     return (
-        <div className={styles["greeting"]}>
+        <div className={styles["invitation-container"]}>
             <InvitationMsg  userName={userName} amount={total}/>
-            {isNotNull && <Link to="/session" className={styles["start-btn"]} >Start now</Link>}
+            {isNotNull && <Link to="/session" className={styles["start-btn"]} >Review all cards</Link>}
         </div>
     )
 }
