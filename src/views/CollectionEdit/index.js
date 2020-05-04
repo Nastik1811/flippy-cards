a0691e5b9462b34c460e643cd187fcd529ee8f24
Nastik1811 <lugovskaya.1999@mail.ru>
2020-05-04 23:28:32 +0300

    Update collection editor

--- src/views/CollectionEdit/index.js
+++ src/views/CollectionEdit/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useContext, useEffect } from 'react'
 import CardItem from './CardItem'
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory, withRouter } from 'react-router-dom'
 import styles from './CollectionEditor.module.scss'
 import { DataContext } from '../../DataManger'
 import EditorWindow from '../../components/EditorWindow'
-import { InputField } from '../../components/FormElements'
+import Loader from '../../components/Loader'
+import {SubmitButton} from '../../components/FormElements'
 
 const CheckablePreview = ({children}) => {
     return(<div>
@@ -12,32 +13,42 @@ const CheckablePreview = ({children}) => {
     </div>)
 } 
 
-const CollectionEdit = () => {
+const CollectionEdit = ({history}) => {
     let {slug} = useParams();
     const {manager} = useContext(DataContext);
 
     const [name, setName] = useState("");
     const [checkedCards, setCheckedCards] = useState([]);
     const [uncheckedCards, setUncheckedCards] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        function fetchData(){
-            manager.getCards(slug).then(setCheckedCards);
-            manager.getCardsWithoutCollection().then(setUncheckedCards);
-            manager.getCollection(slug).then(data => setName(data.name));
-        }
-        fetchData();
+        let nameLoad = manager.getCards(slug).then(setCheckedCards);
+        let cardsLoad = manager.getCardsWithoutCollection().then(setUncheckedCards);
+        let otherCardsLoad = manager.getCollection(slug).then(data => setName(data.name));
+        Promise.all([nameLoad, cardsLoad, otherCardsLoad]).then(() => setIsLoading(false))
     }, [manager, slug])
 
+    const handleSubmit = () => {
+        manager.updateCollection(slug, name);
+        history.push("/manage/collections");
+    }
+
     return (
+        isLoading ? <Loader/> :
         <EditorWindow caption="Collection editor">
-            <header>
-                <h1 className={styles["name"]}>{name}</h1>
-            </header>
-            <div className={styles["cards"]}>
-                    {checkedCards.map(c => <CardItem content={c.content} key={c.id}/>)}
-                    {uncheckedCards.map(c => <CardItem content={c.content} key={c.id}/>)}
-            </div>
+            <form className={styles["content"]} onSubmit={handleSubmit}>
+                <header>
+                    <div className={styles["name_container"]} >
+                        <input className={styles["name"]} value={name} onChange={e => setName(e.target.value)}/>
+                    </div>
+                </header>
+                <div className={styles["cards"]}>
+                        {uncheckedCards.map(c => <CardItem content={c.content} key={c.id}/>)}
+                        {checkedCards.map(c => <CardItem content={c.content} key={c.id}/>)}
+                </div>
+                <SubmitButton label="Save" className={styles["save-btn"]} />
+            </form>
         </EditorWindow>
     )
   
@@ -45,5 +56,4 @@ const CollectionEdit = () => {
 //update collection name
 //update all cards
 
-
-export default CollectionEdit
+export default withRouter(CollectionEdit) 
