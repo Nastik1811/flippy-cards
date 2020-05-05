c75896dc360e916f737984228390b1069922c7d0
Nastik1811 <lugovskaya.1999@mail.ru>
2020-05-05 11:07:13 +0300

    Split Overview component into presentational/container components

--- src/views/Overview/index.js
+++ src/views/Overview/index.js
@@ -1,42 +1,26 @@
-import React, {useState, useEffect, useContext} from 'react'
-import Card from './Card'
+import React, {useState, useEffect, useContext, useCallback} from 'react'
 import {useParams} from 'react-router-dom'
-import styles from './Overview.module.scss'
-import { DataContext, MARK } from '../../DataManger'
+import { DataContext } from '../../DataManger'
 import Loader from "../../components/Loader"
+import Overview from './Overview'
 
-const SessionInfo = ({name, left}) => {
-    return (
-        <div className={styles["session-info"]}>
-            <div className={styles["details"]}>
-                <div className={styles["title"]}>{name}</div>
-                <div className={styles["left"]}>{left} cards left</div>
-            </div>
-        </div>
-    )
-}
-
-const Timer = ({time}) => {
-    return(<div className={styles["passed-time"]}> {time}</div>)
-}
-
-const Overview = ({history}) => {
+const OverviewContainer = ({history}) => {
     const {slug} = useParams();
     const {manager} = useContext(DataContext);
 
     const collection_id = slug ? slug : null;
-    
+
     const [cards, setCards] = useState(null);
     const [currentCardIndex, setCardIndex] = useState(0);
-    const [isFlipped, setFlipped] = useState(false);
     const [left, setLeft] = useState(0);
+    const [time, setTime] = useState(0);
 
     useEffect(()=> {
         manager.getCardsToRecall(collection_id).then(data => {
             if(data.length !== 0){
                 setCards(data);
                 return data;
-            }else{
+            } else{
                 throw Error;
             }
         })
@@ -47,14 +31,16 @@ const Overview = ({history}) => {
         })
     }, [manager, collection_id, history])
 
-    
-    const handleClick = (mark) => {
+    useEffect(() =>{
+        setInterval(() => setTime(c => c + 1), 1000);
+    }, [])
+
+    const handleMarkClick = (mark) => {
         manager.updateCardProgress(cards[currentCardIndex], mark)
 
         if(left !== 0 ){
-            setFlipped(false);
-            setCardIndex(currentCardIndex + 1);
             setLeft(left - 1);
+            setCardIndex(currentCardIndex + 1);
         }
         else{
             alert("That's all. Good job!");
@@ -62,22 +48,18 @@ const Overview = ({history}) => {
         }
     }
 
-    return (
-        cards?
-            <section className={styles["overview"]}>
-                <div className={styles["container"]}>
-                    <SessionInfo name={collection_id ? cards[0].collection.name : "All cards"} left = {left} />
-                    <Card card={cards[currentCardIndex].content} onClick={() => setFlipped(true)} isFlipped={isFlipped}/>
-                    <div className={ isFlipped? styles["buttons-panel"]: styles["buttons-panel-hidden"]} >
-                        <button className="" onClick={() => handleClick(MARK.BAD)}>Fail</button>
-                        <button className="" onClick={() => handleClick(MARK.GOOD)} >Deal</button>
-                        <button className="" onClick={() => handleClick(MARK.EXCELLENT)} >Win</button>
-                    </div>
-                </div>
-            </section>
+    return cards?
+            <Overview
+                name={collection_id ? cards[0].collection.name : "All cards"}
+                currentCard = {cards[currentCardIndex]}
+                onMarkClick ={handleMarkClick}
+                left = {left}
+                time = {time}
+                onTick = {setTime}
+            />
             :
             <Loader/>
-        )
+        
 }
 
-export default Overview;
+export default OverviewContainer;
