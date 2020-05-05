2e9f3fa1a4f652305f6cd788996d8be1b48a6526
Nastik1811 <lugovskaya.1999@mail.ru>
2020-05-05 11:07:56 +0300

    Extract marks panel. Add icons

---
+++ src/views/Overview/Overview/MarksPanel/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './MarksPanel.module.scss'
+import { MARK } from '../../../../DataManger'
+
+const MarksPanel = ({onClick}) => {
+    return(
+    <div className={ styles["panel"]} >
+        <button onClick={() => onClick(MARK.BAD)}><i className={styles["bad"]} /></button>
+        <button onClick={() => onClick(MARK.GOOD)}><i className={styles["good"]} /></button>
+        <button onClick={() => onClick(MARK.EXCELLENT)}><i className={styles["excellent"]} /></button>
+    </div>)
+}
+
+export default MarksPanel;
