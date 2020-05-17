7e390c4ec3f29552d4e0076df66ca92d057057f9
Nastik1811 <lugovskaya.1999@mail.ru>
2020-05-17 22:32:40 +0300

    Finalize collection edit feature
    By now the user could change set of cards collection includes

--- src/views/CollectionEdit/CardItem/index.js
+++ src/views/CollectionEdit/CardItem/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import styles from './CardItem.module.scss'
+import clsx from 'clsx'
 
-const CardItem = ({content}) => {
+const Checkbox = ({isChecked}) => {
+    return( <div 
+                className={clsx(styles["checkbox"],  { [styles["checked"]]: isChecked })}
+                >
+    </div>)
+}
+const CardItem = ({card, isChecked, onClick}) => {
     return(
-        <div className={styles["card-item"]}>
+        <div className={clsx(styles["card-item"],  { [styles["checked"]]: isChecked })} onClick={onClick}>
             <div className={styles["back"]}>
-                {content.back}
+                {card.content.back}
             </div>
             <div className={styles["front"]}>
-                {content.front}
+                {card.content.front}
             </div>
+            <Checkbox isChecked={isChecked} />
         </div>
     )
 }
--- src/views/CollectionEdit/index.js
+++ src/views/CollectionEdit/index.js
@@ -1,51 +1,69 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect, useCallback } from 'react'
 import CardItem from './CardItem'
-import { useParams, useHistory, withRouter } from 'react-router-dom'
+import { useParams, withRouter, Redirect } from 'react-router-dom'
 import styles from './CollectionEditor.module.scss'
 import { DataContext } from '../../DataManger'
 import EditorWindow from '../../components/EditorWindow'
 import Loader from '../../components/Loader'
-import {SubmitButton} from '../../components/FormElements'
+import {SubmitButton, InputField} from '../../components/FormElements'
 
-const CheckablePreview = ({children}) => {
-    return(<div>
-        {children}
-    </div>)
-} 
 
 const CollectionEdit = ({history}) => {
     let {slug} = useParams();
     const {manager} = useContext(DataContext);
 
     const [name, setName] = useState("");
-    const [checkedCards, setCheckedCards] = useState([]);
-    const [uncheckedCards, setUncheckedCards] = useState([]);
+    const [cards, setCards] = useState([]);
+    const [cardStates, setCardStates] = useState({})
+    //const [uncheckedCards, setUncheckedCards] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [completed, setCompleted] = useState(false);
+
 
     useEffect(() => {
-        let nameLoad = manager.getCards(slug).then(setCheckedCards);
-        let cardsLoad = manager.getCardsWithoutCollection().then(setUncheckedCards);
-        let otherCardsLoad = manager.getCollection(slug).then(data => setName(data.name));
-        Promise.all([nameLoad, cardsLoad, otherCardsLoad]).then(() => setIsLoading(false))
+        let nameLoad =  manager.getCollection(slug).then(data => setName(data.name));
+        let cardsLoad = manager.getCardsForCollectionEdit(slug).then(data => {
+            setCards(data)
+            let states = data.reduce((obj, item) => {
+                return {
+                    ...obj,
+                    [item.id]: item.inCollection
+                }
+            }, {})
+            setCardStates(states)
+        })
+        Promise.all([nameLoad, cardsLoad]).then(() => setIsLoading(false))
     }, [manager, slug])
 
     const handleSubmit = () => {
-        manager.updateCollection(slug, name);
-        history.push("/manage/collections");
+        manager.updateCollection(slug, name, cardStates)
+        console.log(cardStates)
+        setCompleted(true);
     }
+
+    if(completed){
+        return <Redirect to="/manage/collections"/>
+      }
+  
+      const onCheck = id => {
+        setCardStates({
+            ...cardStates,
+            [id]: !cardStates[id]
+        })
+      }
 
     return (
         isLoading ? <Loader/> :
-        <EditorWindow caption="Collection editor">
+        <EditorWindow caption="Collection editor" onReturn={() => setCompleted(true)}>
             <form className={styles["content"]} onSubmit={handleSubmit}>
                 <header>
-                    <div className={styles["name_container"]} >
-                        <input className={styles["name"]} value={name} onChange={e => setName(e.target.value)}/>
+                    <div className={styles["name-container"]} >
+                        <InputField 
+                            className={styles["name"]} value={name} onChange={setName}/>
                     </div>
                 </header>
                 <div className={styles["cards"]}>
-                        {uncheckedCards.map(c => <CardItem content={c.content} key={c.id}/>)}
-                        {checkedCards.map(c => <CardItem content={c.content} key={c.id}/>)}
+                        {cards.map(c => <CardItem card={c} key={c.id} isChecked={cardStates[c.id]} onClick={() => onCheck(c.id)}/>)}
                 </div>
                 <SubmitButton label="Save" className={styles["save-btn"]} />
             </form>
